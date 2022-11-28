5eb396a471aca7056a473298992e9dbdc2de589e
Vladislav Krutikov <Eagleheard5@gmail.com>
2022-11-28 12:12:08 +0300

    fix: fix bugs with authorization

--- src/api/authorization.ts
+++ src/api/authorization.ts
@@ -3,13 +3,13 @@ import axios from "axios";
 import { IUser } from "types/interfaces";
 
 export const registration = (params: IUser) => {
-  return axios.post(`http://localhost:5000/register`, params, {
+  return axios.post(`http://localhost:5000/api/register`, params, {
     withCredentials: true,
   });
 };
 
 export const login = (params?: IUser) => {
-  return axios.post(`http://localhost:5000/login`, params, {
+  return axios.post(`http://localhost:5000/api/login`, params, {
     withCredentials: true,
     headers: {
       "Access-Control-Allow-Origin": "*",
@@ -19,13 +19,9 @@ export const login = (params?: IUser) => {
 };
 
 export const authorization = () => {
-  return axios.get(`http://localhost:5000/auth`, {
+  return axios.get(`http://localhost:5000/api/auth`, {
     headers: {
       Authorization: `Bearer ${sessionStorage.getItem("token")}`,
     },
   });
-};
-
-export const logout = () => {
-  return axios.put("http://localhost:5000/logout");
 };
--- src/api/fetchUserInfo.ts
+++ src/api/fetchUserInfo.ts
@@ -1,11 +1,15 @@
 import axios from "axios";
 
 export const fetchUserInfo = (email?: string) => {
-  return axios.get(`http://localhost:5000/profile/${email}`);
+  return axios.get(`http://localhost:5000/api/${email}`, {
+    headers: {
+      Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+    },
+  });
 };
 
 export const updateUserInfo = (description?: string, email?: string) => {
-  return axios.put(`http://localhost:5000/update/${email}`, {
+  return axios.put(`http://localhost:5000/api/${email}`, {
     description: description,
   });
 };
